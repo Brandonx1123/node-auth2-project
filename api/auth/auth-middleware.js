2918596e6a29a2ac3c95c6e4d3882c6c544f0b2b
brandon santos <brandonx1123@gmail.com>
2021-03-23 20:05:51 -0400

    fixed tests mvp done

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -21,11 +21,11 @@ const restricted = (req, res, next) => {
   const token = req.headers.authorization;
 
   if (!token) {
-    res.status(401).json({ message: "Token is required to proceed" });
+    res.status(401).json({ message: "Token required" });
   } else {
     jwt.verify(token, JWT_SECRET, (err, decoded) => {
       if (err) {
-        res.status(401).json({ message: "Token is invalid" });
+        res.status(401).json({ message: "Token invalid" });
       } else {
         req.decodedJwt = decoded;
         next();
